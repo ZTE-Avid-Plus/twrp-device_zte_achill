2a33c3238161f18f10c7003ee26ad2b2f8b6b698
alexenferman <alextud0132@gmail.com>
2021-02-05 23:07:38 -0500

    Add Init boot Alarm Support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,11 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_msm8909
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8909
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
